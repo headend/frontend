f11a18eb9741d2361ac1a299d90725a00867f010
thuonghh <thuonghh@vng.com.vn>
2020-12-13 23:29:52 +0700

    move doc file to document

--- makemigration.sh
+++
@@ -1,5 +0,0 @@
-python manage.py makemigrations channel
-python manage.py makemigrations iptvresource
-python manage.py makemigrations device
-python manage.py makemigrations agent
-python manage.py makemigrations iptvprofile
--- migrate.sh
+++
@@ -1,6 +0,0 @@
-python manage.py migrate channel
-python manage.py migrate iptvresource
-python manage.py migrate device
-python manage.py migrate agent
-python manage.py migrate iptvprofile
-python manage.py migrate
