a6468b3ddf25c5b33983b5c6c97702af94b19a46
Nguyen Huy <tronghuy02@gmail.com>
2020-12-13 17:08:26 +0700

    add agents template

--- agent/views.py
+++ agent/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+import time
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+import logging
+from datetime import timedelta
+
+
+def index(request):
+    template = loader.get_template('agents/index.html')
+    data = None
+    rc= None
+    msg = ""
+    context = {
+        'data': data,
+        'rc': rc,
+        'msg': msg,
+    }
+    return HttpResponse(template.render(context, request))
 
 # Create your views here.
