8b594ac6b8a75ed92de432793d23055de7e6a8b7
thuonghh <thuonghh@vng.com.vn>
2020-12-13 19:26:26 +0700

    migrations sequence

---
+++ makemigration.sh
@@ -0,0 +1,5 @@
+python manage.py makemigrations channel
+python manage.py makemigrations iptvresource
+python manage.py makemigrations device
+python manage.py makemigrations agent
+python manage.py makemigrations iptvprofile
