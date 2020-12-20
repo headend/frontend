ba544723514ee4dd4d4687722c93cfe42b80db75
Nguyen Huy <tronghuy02@gmail.com>
2020-12-20 11:52:30 +0700

    update url agent for index

--- iptv_frontend/urls.py
+++ iptv_frontend/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import agent
-from . import views
+from agent import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
