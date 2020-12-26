09b18d76a2a7c738f54ced8ed3f89f20193602d2
Nguyen Huy <tronghuy02@gmail.com>
2020-12-26 16:19:51 +0700

    update sync status for monitor

--- iptvprofile/views.py
+++ iptvprofile/views.py
@@ -3,14 +3,40 @@ from django.core import serializers
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseRedirect
+from django.http import JsonResponse
 from iptvprofile.models import Monitor
+from django.db.models import F
+import json
 
 # Create your views here.
 def index(request):
     template = loader.get_template('iptvprofile/index.html')
-    data = Monitor.objects.select_related('agent','profile')
-    print(serializers.serialize('json',data))
+    data = Monitor.objects.select_related('agent','profile').values('id','status',location=F('agent__location'),mulip=F('profile__multicast_ip__ip'),channel=F('profile__channel__name'),
+    sigmonitor=F("signal_monitor"),vidmonitor=F('video_monitor'),vidstatus=F('status_video'),audmonitor=F('audio_monitor'), audstatus=F('status_audio'),isenable=F('is_enable'),
+    )
+    # data = list(data)
+    print(data)
     context = {
         'data': data
     }
     return HttpResponse(template.render(context, request))
+
+def getId(request):
+    if request.method == "GET":
+        print("aaa")
+        data= list(Monitor.objects.values('id'))
+        # print(serializers.serialize('json',data))
+        return JsonResponse(data, safe=False)
+        # return HttpResponse('sdfdf',status=200)
+    else:
+        return HttpResponse(status=500)
+
+def updateStatus(request):
+    if request.method == "GET":
+        data = list(Monitor.objects.select_related('agent','profile').values('id','status',location=F('agent__location'),mulip=F('profile__multicast_ip__ip'),chanle=F('profile__channel__name'),
+        sigmonitor=F("signal_monitor"),vidmonitor=F('video_monitor'),vidstatus=F('status_video'),audmonitor=F('audio_monitor'), audstatus=F('status_audio'),isenable=F('is_enable'),
+        ))
+        return HttpResponse(json.dumps(data),status=200,content_type="application/json")
+    else:
+        return HttpResponse(status=500)
+    # return JsonResponse(data=data, safe=False)
---
+++ static/homepage/js/proflie.js
@@ -0,0 +1,128 @@
+$(document).ready(function() {
+    update_status();
+});
+
+function sync_status(id, val){
+    // console.log(val)
+    if (val == 1){
+        $('#status-'+id).removeClass("bi-check-circle-fill");
+        $('#status-'+id).removeClass("bi-x-circle-fill");
+        $('#status-'+id).addClass("bi-check-circle-fill");
+    }
+    else {
+        $('#status-'+id).removeClass("bi-check-circle-fill");
+        $('#status-'+id).removeClass("bi-x-circle-fill"); 
+        $('#status-'+id).addClass("bi-x-circle-fill");
+    }
+}
+
+function sync_vidstatus(id, val){
+    // console.log(val)
+    if (val == 1){
+        $('#vidstatus-'+id).removeClass("bi-check-circle-fill");
+        $('#vidstatus-'+id).removeClass("bi-x-circle-fill");
+        $('#vidstatus-'+id).addClass("bi-check-circle-fill");
+        // console.log(id)
+    }
+    else {
+        $('#vidstatus-'+id).removeClass("bi-check-circle-fill");
+        $('#vidstatus-'+id).removeClass("bi-x-circle-fill"); 
+        $('#vidstatus-'+id).addClass("bi-x-circle-fill");
+    }
+}
+
+function sync_audstatus(id, val){
+    // console.log(val)
+    if (val == 1){
+        $('#audstatus-'+id).removeClass("bi-check-circle-fill");
+        $('#audstatus-'+id).removeClass("bi-x-circle-fill");
+        $('#audstatus-'+id).addClass("bi-check-circle-fill");
+        // console.log(id)
+    }
+    else {
+        $('#audstatus-'+id).removeClass("bi-check-circle-fill");
+        $('#audstatus-'+id).removeClass("bi-x-circle-fill"); 
+        $('#audstatus-'+id).addClass("bi-x-circle-fill");
+    }
+}
+
+function sync_isenable(id, val){
+    // console.log(val)
+    if (val == 1){
+        $('#isenable-'+id).removeClass("bi-check-circle-fill");
+        $('#isenable-'+id).removeClass("bi-x-circle-fill");
+        $('#isenable-'+id).addClass("bi-check-circle-fill");
+        // console.log(id)
+    }
+    else {
+        $('#isenable-'+id).removeClass("bi-check-circle-fill");
+        $('#isenable-'+id).removeClass("bi-x-circle-fill"); 
+        $('#isenable-'+id).addClass("bi-x-circle-fill");
+    }
+}
+
+function sync_sigmonitor(id, val){
+    // console.log(val)
+    if (val == 1){
+        $('#sigmonitor-'+id).removeClass("bi-toggle-off");
+        $('#sigmonitor-'+id).removeClass("bi-toggle-on");
+        $('#sigmonitor-'+id).addClass("bi-toggle-on");
+        // console.log(id)
+    }
+    else {
+        $('#sigmonitor-'+id).removeClass("bi-toggle-off");
+        $('#sigmonitor-'+id).removeClass("bi-toggle-on"); 
+        $('#sigmonitor-'+id).addClass("bi-toggle-off");
+    }
+}
+
+function sync_vidmonitor(id, val){
+    // console.log(val)
+    if (val == 1){
+        $('#vidmonitor-'+id).removeClass("bi-toggle-off");
+        $('#vidmonitor-'+id).removeClass("bi-toggle-on");
+        $('#vidmonitor-'+id).addClass("bi-toggle-on");
+        // console.log(id)
+    }
+    else {
+        $('#vidmonitor-'+id).removeClass("bi-toggle-off");
+        $('#vidmonitor-'+id).removeClass("bi-toggle-on"); 
+        $('#vidmonitor-'+id).addClass("bi-toggle-off");
+    }
+}
+
+function sync_audmonitor(id, val){
+    // console.log(val)
+    if (val == 1){
+        $('#audmonitor-'+id).removeClass("bi-toggle-off");
+        $('#audmonitor-'+id).removeClass("bi-toggle-on");
+        $('#audmonitor-'+id).addClass("bi-toggle-on");
+        // console.log(id)
+    }
+    else {
+        $('#audmonitor-'+id).removeClass("bi-toggle-off");
+        $('#audmonitor-'+id).removeClass("bi-toggle-on"); 
+        $('#audmonitor-'+id).addClass("bi-toggle-off");
+    }
+}
+
+function update_status(){
+    var sync = function(){
+        $.get('/monitor/upstatus/',function(result,status){
+        console.log(result);
+        if (status == 'success'){
+             $.each(result,function(index,dicts){
+                 sync_status(dicts.id,dicts.status);
+                 sync_vidstatus(dicts.id, dicts.vidstatus);
+                 sync_vidmonitor(dicts.id,dicts.vidmonitor);
+                 sync_audmonitor(dicts.id, dicts.audmonitor);
+                 sync_audstatus(dicts.id, dicts.audstatus);
+                 sync_sigmonitor(dicts.id, dicts.sigmonitor);
+                 sync_isenable(dicts.id, dicts.isenable);
+             });
+        }
+    });
+    };
+    setInterval(sync,10000);
+    // setTimeout(sync, 2000);
+}
