18cab7f54f54b88dde97e6ebf2413c4337469b70
Nguyen Huy <tronghuy02@gmail.com>
2020-12-28 01:05:46 +0700

    update api-front gateway

--- iptv_frontend/config.py
+++ iptv_frontend/config.py
@@ -5,16 +5,16 @@ WORKER={
         "0":"/api/v1/ctl/127.0.0.1/master/stop-worker",
     },
     "signal":{
-        "1":"/api/v1/ctl/127.0.0.1/master/signal/enable",
-        "0":"/api/v1/ctl/127.0.0.1/master/signal/disable",
+        "1":"/api/v1/ctl/127.0.0.1/master/monitor/signal/enable",
+        "0":"/api/v1/ctl/127.0.0.1/master/monitor/signal/disable",
     },
     "video" : {
-        "1":"/api/v1/ctl/127.0.0.1/master/video/enable",
-        "0":"/api/v1/ctl/127.0.0.1/master/video/disable",
+        "1":"/api/v1/ctl/127.0.0.1/master/monitor/video/enable",
+        "0":"/api/v1/ctl/127.0.0.1/master/monitor/video/disable",
     },
     "audio": {
-        "1":"/api/v1/ctl/127.0.0.1/master/audio/enable",
-        "0":"/api/v1/ctl/127.0.0.1/master/audio/disable",
+        "1":"/api/v1/ctl/127.0.0.1/master/monitor/audio/enable",
+        "0":"/api/v1/ctl/127.0.0.1/master/monitor/audio/disable",
     },
-   "thread":"/api/v1/ctl/127.0.0.1/master/start-worker"
+   "thread":"/api/v1/ctl/127.0.0.1/master/monitor/start-worker"
 }
