4cba963ad2e20ef4ce5d678d640778957bb396ce
Nguyen Huy <tronghuy02@gmail.com>
2021-01-21 13:05:32 +0700

    them function add new monitor and detele

--- static/homepage/js/monitor.js
+++ static/homepage/js/monitor.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     update_status();
+    loadForeignKey();
 });
 
 function sync_status(id, val){
@@ -133,6 +134,128 @@ function update_status(){
         }
     });
     };
-    setInterval(sync,10000);
+    // setInterval(sync,10000);
     // setTimeout(sync, 2000);
+}
+
+function loadSelectProfile(lstValuses){
+    $.each(lstValuses, function(index,values){
+        $('#frm-add-profile').prop('selectedIndex',0);
+        if($('#frm-add-profile').find("option[value='"+values.state+"']").length){
+            $('#frm-add-profile').trigger('change');
+         }else{
+             $('#frm-add-profile').append($('<option>', {
+                 value:values.id,
+                 text: values.channelname+'-'+values.quality,
+             }));
+             $('#frm-add-profile').trigger('change');
+        }
+         // console.log($('#frm-add-status').val());
+    });
+}
+function loadSelectAgent(lstValuses){
+    $.each(lstValuses, function(index,values){
+        $('#frm-add-agent').prop('selectedIndex',0);
+        if($('#frm-add-agent').find("option[value='"+values.state+"']").length){
+            $('#frm-add-agent').trigger('change');
+         }else{
+             $('#frm-add-agent').append($('<option>', {
+                 value:values.id,
+                 text: values.location+'-'+values.ip,
+             }));
+             $('#frm-add-agent').trigger('change');
+        }
+         // console.log($('#frm-add-status').val());
+    });
+}
+function loadSelectStatus(lstValuses){
+    $.each(lstValuses, function(index,values){
+        $('#frm-add-status').prop('selectedIndex',0);
+        if($('#frm-add-status').find("option[value='"+values.state+"']").length){
+            $('#frm-add-status').trigger('change');
+         }else{
+             $('#frm-add-status').append($('<option>', {
+                 value:values.state,
+                 text: values.name,
+             }));
+             $('#frm-add-status').trigger('change');
+        }
+         // console.log($('#frm-add-status').val());
+    });
+}
+function loadForeignKey(){
+    $.get("/monitor/get4create/", function(result, status){
+        // console.log(status);
+      }).done(function(result){
+          loadSelectProfile(result.Profile);
+          loadSelectAgent(result.Agent);
+          loadSelectStatus(result.Status);
+      }).fail(function(result, textStatus, xhr){
+        console.log(result.status, textStatus, xhr);
+      });
+    // loadDataSelect("frm-add-vlan");
+}
+function showModalMsg(msg, cls){
+    $('#lbl-message').removeAttr('class');
+    $('#lbl-message').addClass(cls);
+    $('#lbl-message').text(msg);
+    $('#msgModal').modal('toggle');
+}
+$("#btnnewmontior-form-submit").click(function (e){
+    frmdata = {
+        "profile": $('#frm-add-profile').val(),
+        "agent": $('#frm-add-agent').val(),
+        "status": $('#frm-add-status').val(),
+        "enable": $('#chkEnable').prop("checked")?1:0,
+        "video": $('#chkVideo').prop("checked")?1:0,
+        "audio": $('#chkAudio').prop("checked")?1:0,
+        "signal": $('#chkSignal').prop("checked")?1:0,
+    };
+    e.preventDefault();
+    console.log(frmdata);
+    $.post('/monitor/newmonitor/', frmdata, function(reult){
+         console.log(reult);
+         showModalMsg("Success!", "text-success");
+         $('#msgModal .closed').click(function (e){
+             e.preventDefault();
+             location.reload();
+         });
+     }).fail(function (resulf,msg,xhr){
+         console.log(resulf.responseText);
+         showModalMsg(resulf.responseText, "text-danger");
+         // $('#msgModal').modal('toggle');
+     });
+});
+function deleteMonitor(select_obj) {
+  $('#loading').show();
+  if (confirm("You delelte this profile: "+select_obj)){
+      $.ajax({
+        url:"/monitor/delete/"+select_obj,
+        method: 'DELETE',
+        contentType: 'application/json',
+        success: function(result,status) {
+            // handle success
+            console.log(status);
+            // alert(status);
+            showModalMsg(msg=result.responseText,'text-success')
+            $('#msgModal .closed').click(function (e){
+             e.preventDefault();
+             location.reload();
+             });
+            $('#loading').hide();
+        },
+        error: function(request,status,error) {
+            // handle failure
+            // alert(msg);
+            showModalMsg(msg=request.responseText,'text-danger')
+            $('#loading').hide();
+        }
+    });
+      $('#msgModal .close').click(function (e){
+          e.preventDefault();
+          location.reload();
+      });
+  }else{
+      $('#loading').hide();
+  }
 }
