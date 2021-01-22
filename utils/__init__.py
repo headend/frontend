fe2fea472b521251b353c84a1cd045808ca95a0b
thuonghh <thuonghh@vng.com.vn>
2021-01-22 11:32:39 +0700

    caculae downtime methob

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
+import datetime
 # Python Program to Convert seconds 
 # into hours, minutes and seconds 
   
@@ -8,4 +9,8 @@ def convert_seconds_to_day(seconds):
     seconds %= 3600
     minutes = seconds // 60
     seconds %= 60
-    return "%dd %02dh:%02dm:%02ds" % (days, hour, minutes, seconds) 
+    return "%dd %02dh:%02dm:%02ds" % (days, hour, minutes, seconds)
+
+def caculate_distance(date_update):
+    now = datetime.datetime.now().strftime("%s")
+    return int(now) - int(date_update.strftime("%s"))
